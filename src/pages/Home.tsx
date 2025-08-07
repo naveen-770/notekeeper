243e3bad408d85d3941281192e079565e3781140
Naveen <naveensreevalsan@gmail.com>
2025-08-07 19:38:43 +0530

    Display list of notes on Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,29 @@
+import { useNotes } from "../context/NotesContext";
+import { Link } from "react-router-dom";
 const Home = () => {
-  return <h1>All Notes</h1>;
+  const { notes } = useNotes();
+  return (
+    <div>
+      <h1>All Notes</h1>
+
+      {notes.length === 0 && <p>No notes available. Create one!</p>}
+
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            <Link to={`/note/${note.id}`}>
+              <h2>{note.title}</h2>
+              <p>{note.body.substring(0, 100)}...</p>
+
+              {note.tags.map((tag) => (
+                <li key={tag.id}>{tag.label}</li>
+              ))}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
